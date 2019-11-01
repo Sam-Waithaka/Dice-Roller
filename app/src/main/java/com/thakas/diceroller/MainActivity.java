62ab2934e588da4cdfedebcdbf1250a2b69e2adc
Sthakas <sw.waithakasamuel@gmail.com>
2019-11-01 12:00:13 +0300

    A dice rolling app.
    Signed-off-by: Sthakas <sw.waithakasamuel@gmail.com>

--- app/src/main/java/com/thakas/diceroller/MainActivity.java
+++ app/src/main/java/com/thakas/diceroller/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -33,21 +34,27 @@ public class MainActivity extends AppCompatActivity {
         switch (rollNummber){
             case 1:
                 diceImage.setImageResource(R.drawable.dice_1);
+                Toast.makeText(this, "one", Toast.LENGTH_SHORT).show();
                 break;
             case 2:
                 diceImage.setImageResource(R.drawable.dice_2);
+                Toast.makeText(this, "two", Toast.LENGTH_SHORT).show();
                 break;
             case 3:
                 diceImage.setImageResource(R.drawable.dice_3);
+                Toast.makeText(this, "three", Toast.LENGTH_SHORT).show();
                 break;
             case 4:
                 diceImage.setImageResource(R.drawable.dice_4);
+                Toast.makeText(this, "four", Toast.LENGTH_SHORT).show();
                 break;
             case 5:
                 diceImage.setImageResource(R.drawable.dice_5);
+                Toast.makeText(this, "five", Toast.LENGTH_SHORT).show();
                 break;
             case 6:
                 diceImage.setImageResource(R.drawable.dice_6);
+                Toast.makeText(this, "six", Toast.LENGTH_SHORT).show();
                 break;
                 default:
                     diceImage.setImageResource(R.drawable.empty_dice);
